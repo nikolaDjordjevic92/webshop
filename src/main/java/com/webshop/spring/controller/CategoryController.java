7fe65c8fa9f6f4737b0f9c1db5f2d35ff8df5680
filip <djordjevic.filip994@gmail.com>
2017-11-03 17:49:25 +0100

    jackson added

--- src/main/java/com/webshop/spring/controller/CategoryController.java
+++ src/main/java/com/webshop/spring/controller/CategoryController.java
@@ -35,7 +35,6 @@ public class CategoryController {
 	
 	@RequestMapping(value="/categories/{id}")
 	public String getCategory(ModelMap map, @PathVariable Integer id) {
-		
 		List<Product> products=productManager.getProductByCategoryId(id);
 		map.addAttribute("products",products);
 		List<Category> categories=categoryManager.getAllCategories();
